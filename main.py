d14b6406cc0cd7295a163f5ff1d82acf2325b6a8
Finn Glück <f.glueck@campus.tu-berlin.de>
2025-01-14 13:29:43 +0100

    Refactor style

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-import numpy as np
+import numpy
 
-
-def main():
-    array = np.array([[1, 2, 3, 4],
-                      [5, 6, 7, 8],
-                      [9, 10, 11, 12]])
-
-    print("\nArray:")
-    print(array)
-
-    print("\nArray[:, :]:")
-    print(array[:, :])
-
-    print("\nArray[:, 1:]:")
-    print(array[:, 1:])
-
-    print("\nArray[:, :1]:")
-    print(array[:, :1])
-
-
-main()
+print(numpy.arange(0, 1, 1/9))
